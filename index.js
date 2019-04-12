95fa4f45dd3f5430481ed89b3eb0459068c25ff0
valentinachavez <valencha10@gmail.com>
2019-04-12 08:00:24 -0500

    Funciona Handlebars

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ var exphbs= require('express-handlebars');
 
 
 var app= express();
-app,use(express.static('public'));
+app.use(express.static('public'));
 
 app.engine('handlebars',exphbs());
 app.set('view engine', 'handlebars');
+
+
+app.get('/', function(request, response){
+    var contexto= {
+    titulo : 'pagina',
+    };
+    response.render('home',contexto);
+});
+
+app.listen(3000);
